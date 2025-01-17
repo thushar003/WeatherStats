db66e96cad300f7c1b00cce6406174401649affa
thushar003 <thusharjoji2@gmail.com>
2025-01-16 20:32:57 -0500

    Temporary strings representing various weather states added as list items, API to be integrated (eventually)

--- app/src/main/java/com/example/weatherstats/MainActivity.kt
+++ app/src/main/java/com/example/weatherstats/MainActivity.kt
@@ -4,19 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.weatherstats.ui.theme.WeatherStatsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        //TODO temporary weather status strings, after integrating a weather API, this will most likely be taken out
+        val weatherStatus = listOf("Sunny","Rainy","Cloudy","Snowy","Humid","Windy");
+
         setContent {
             WeatherStatsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -24,8 +34,30 @@ class MainActivity : ComponentActivity() {
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+                    LazyColumn {
+                        items(weatherStatus)
+                        {
+                            ListItem(it)
+                        }
+                    }
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun ListItem(name: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(12.dp)
+    ) {
+        Row {
+            Text(
+                text = name,
+                modifier = Modifier.padding(24.dp)
+            )
         }
     }
 }
